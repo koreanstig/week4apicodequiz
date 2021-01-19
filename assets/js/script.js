da58f05484c6df76ad911b0fe67525c01aa941d9
Jung Lee <koreanstig@gmail.com>
2021-01-18 19:45:46 -0800

    added some notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
+// instead of adding a class to all list in questions, i used a for loop
 for (var i = 0; i<document.querySelectorAll("#li").length; i++) {
     document.querySelectorAll("#li")[i].classList.add("highscorebtn", "btn-primary", "btn-sm", "btn");
 }
+// i used another for loop to keep the answers in one column
 for (var i = 0; i<document.querySelectorAll("ol.answers").length; i++) {
     document.querySelectorAll("ol.answers")[i].classList.add("col-4");
 }
-var timerElement = document.querySelector(".timer-count");
 
+var timerElement = document.querySelector(".timer-count");
 var timerCount;
 
+// this function is what allows the first question to appear and timer to start clocking down
 function start() {
     document.getElementById("q1").style.display = "block";
     timerCount = 75;
@@ -18,27 +21,9 @@ function timeStart (){
     timer = setInterval(function() {
         timerCount--;
         timerElement.textContent = timerCount;
-        if (timerCount >= 0) {
-        //   // Tests if win condition is met
-        //   if (isWin && timerCount > 0) {
-        //     // Clears interval and stops timer
-            clearInterval(timer);
-        //     winGame();
-          }
-        // }
-        // // Tests if time has run out
-        if (timerCount === 0) {
-        //   // Clears interval
-          clearInterval(timer);
-        //   loseGame();
-        }
       }, 1000);
-    
-    // for (i=75; i>0; i--){
-    //     document.querySelector(".timer-count").innerHTML = i;
-    // }
 };
 
-
+// this function starts the quiz
 start();
 
