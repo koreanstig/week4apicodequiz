533a16c8874bba8d53be9e032f9583d74d0b5fa6
Jung Lee <koreanstig@gmail.com>
2021-01-18 20:46:45 -0800

    added some javascript to get to next question, also added a score keeper on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,24 @@
 for (var i = 0; i<document.querySelectorAll("#li").length; i++) {
     document.querySelectorAll("#li")[i].classList.add("highscorebtn", "btn-primary", "btn-sm", "btn");
 }
+
 // i used another for loop to keep the answers in one column
 for (var i = 0; i<document.querySelectorAll("ol.answers").length; i++) {
     document.querySelectorAll("ol.answers")[i].classList.add("col-4");
 }
 
+// variables for timer
 var timerElement = document.querySelector(".timer-count");
 var timerCount;
+
+var highScore = 0;
 
 // this function is what allows the first question to appear and timer to start clocking down
 function start() {
     document.getElementById("q1").style.display = "block";
     timerCount = 75;
     timeStart();
+    correct();
 };
 
 function timeStart (){
@@ -23,6 +28,32 @@ function timeStart (){
         timerElement.textContent = timerCount;
       }, 1000);
 };
+
+function nextQuestion(){
+    setInterval(function(){
+        document.querySelector('.correct').style.display = "block";
+    }, 0);
+    setInterval(function(){
+        document.getElementById("q1").style.display = "none";
+    }, 1000);
+    setInterval(function(){
+        document.getElementById("q2").style.display = "block";
+    }, 1000);
+    setInterval(function(){
+        document.querySelector('.correct').style.display = "none";
+    }, 1000);
+    
+}
+
+function correct(){
+    for (var i = 0; i<document.querySelectorAll("li.answer1").length; i++) {
+            document.querySelectorAll("li.answer1")[2].addEventListener("click", function(){
+            nextQuestion();
+        })
+    }
+    highScore += 10;
+};
+
 
 // this function starts the quiz
 start();
