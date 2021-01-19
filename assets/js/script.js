4816ecdcc03b757e84d0213cea45a763eb80f418
Jung Lee <koreanstig@gmail.com>
2021-01-18 19:16:08 -0800

    added a function that starts the quiz and shows the 1st question upon click, also created new divs for correct and wrong prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,32 +8,8 @@ for (var i = 0; i<document.querySelectorAll("ol.answers").length; i++) {
 
 
 function start() {
-    var startQuiz = document.querySelector(".startQuiz").addEventListener("click", function(){
-
-    });
+    document.getElementById("q1").style.display = "block";
 }
 
+start();
 
-// var timerElement = document.querySelector(".timer-count");
-
-// function startTimer() {
-//   // Sets timer
-//   timer = setInterval(function() {
-//     timerCount--;
-//     timerElement.textContent = timerCount;
-//     if (timerCount >= 0) {
-//       // Tests if win condition is met
-//       if (isWin && timerCount > 0) {
-//         // Clears interval and stops timer
-//         clearInterval(timer);
-//         winGame();
-//       }
-//     }
-//     // Tests if time has run out
-//     if (timerCount === 0) {
-//       // Clears interval
-//       clearInterval(timer);
-//       loseGame();
-//     }
-//   }, 1000);
-// }
