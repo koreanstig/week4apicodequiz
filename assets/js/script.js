ac894069236f525da1cc958d69c2777e0dccc336
Jung Lee <koreanstig@gmail.com>
2021-01-18 17:09:28 -0800

    redid the timer so that they're in a row/col via bootstrap so that I may control the timer div separate to the string timer

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var timerElement = document.querySelector(".timer-count");
+
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
