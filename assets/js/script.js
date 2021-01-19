4383701e310f759903f2373596a11ab6c592bad2
Jung Lee <koreanstig@gmail.com>
2021-01-18 18:24:14 -0800

    added some js to give the li and ol elements some classes from bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
-var timerElement = document.querySelector(".timer-count");
-
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
+for (var i = 0; i<document.querySelectorAll("li").length; i++) {
+    document.querySelectorAll("li")[i].classList.add("highscorebtn", "btn-primary", "btn-sm", "btn");
 }
+
+for (var i = 0; i<document.querySelectorAll("ol").length; i++) {
+    document.querySelectorAll("ol")[i].classList.add("col-4");
+}
+
+
+
+// var timerElement = document.querySelector(".timer-count");
+
+// function startTimer() {
+//   // Sets timer
+//   timer = setInterval(function() {
+//     timerCount--;
+//     timerElement.textContent = timerCount;
+//     if (timerCount >= 0) {
+//       // Tests if win condition is met
+//       if (isWin && timerCount > 0) {
+//         // Clears interval and stops timer
+//         clearInterval(timer);
+//         winGame();
+//       }
+//     }
+//     // Tests if time has run out
+//     if (timerCount === 0) {
+//       // Clears interval
+//       clearInterval(timer);
+//       loseGame();
+//     }
+//   }, 1000);
+// }
